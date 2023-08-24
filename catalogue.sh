318ac092ea090dafc6b86f98d97d6f8bd9f965d8
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-08-24 23:05:48 +0100

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ yum install nodejs -y
 #useradd roboshop
 mkdir -p /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -f /app/*
+rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
 cd /app
