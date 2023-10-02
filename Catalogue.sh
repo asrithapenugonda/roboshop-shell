00d9c897252e05fcf48f5fe5a0dd0005f5ed5334
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-10-02 13:34:09 +0100

    catalogue script

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,23 +1,17 @@
 script_location=$(pwd)
-
-set -e
-
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+dnf install nodejs -y
+useradd roboshop
+mkdir /app
 
-yum install nodejs -y
-
-#useradd roboshop
-mkdir -p /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
 cd /app
 unzip /tmp/catalogue.zip
+
 cd /app
 npm install
 
-cp ${script_location}/Files/catalogue.service /etc/systemd/system/catalogue.service
-
+cp ${script_location}/Files/Catalogue.service /etc/systemd/system/Catalogue.service
 systemctl daemon-reload
-
 systemctl enable catalogue
 systemctl start catalogue
