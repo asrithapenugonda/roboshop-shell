1cef2f3cb28b20ede41714f922f6749a56e44739
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-10-09 16:41:57 +0100

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,16 @@
 script_location=$(pwd)
 
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+###curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+sudo dnf remove nodejs
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | sudo bash -
+sudo dnf install nodejs -y
+
+node -v
+npm -v
 
 
-dnf install nodejs -y
+###dnf install nodejs -y
 useradd roboshop
 mkdir /app
 
