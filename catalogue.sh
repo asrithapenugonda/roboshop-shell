7d1f55b009cc6564f57a365109bbc1bf5ed40587
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-10-26 15:25:46 +0100

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,29 +1,39 @@
+#!/bin/bash
+
+# Store the current working directory
 script_location=$(pwd)
 
-###curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+# Install the required packages
+sudo dnf install -y gcc-c++
 
-
+# Remove the existing Node.js (if any)
 sudo dnf remove nodejs -y
+
+# Download and extract Node.js
 wget https://nodejs.org/dist/v14.18.1/node-v14.18.1-linux-x64.tar.xz
 tar -xf node-v14.18.1-linux-x64.tar.xz
 sudo cp -r node-v14.18.1-linux-x64/* /usr/local/
+rm -rf node-v14.18.1-linux-x64.tar.xz node-v14.18.1-linux-x64
 
+# Check Node.js and npm versions
 node -v
 npm -v
 
+# Create a user for your application
+sudo useradd roboshop
 
-###dnf install nodejs -y
-useradd roboshop
-mkdir /app
+# Create the application directory
+sudo mkdir -p /app
 
+# Download the application code
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
+sudo unzip /tmp/catalogue.zip -d /app
+rm -f /tmp/catalogue.zip
 
-cd /app
-npm install
+# Copy the service file
+sudo cp ${script_location}/Files/catalogue.service /etc/systemd/system/Catalogue.service
 
-cp ${script_location}/Files/catalogue.service /etc/systemd/system/Catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
+# Reload systemd and enable/start the service
+sudo systemctl daemon-reload
+sudo systemctl enable catalogue
+sudo systemctl start catalogue
