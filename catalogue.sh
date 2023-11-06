ae369c18d09475ded01ef92867dc4f41438f3556
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-11-06 10:21:55 +0000

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -28,9 +28,15 @@ sudo useradd roboshop
 sudo mkdir -p /app
 
 # Download the application code
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-sudo unzip /tmp/catalogue.zip -d /app
-rm -f /tmp/catalogue.zip
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+rm -rf /app/*
+cd /app
+unzip /tmp/catalogue.zip
+cd /app
+npm install
+
+
+
 
 # Copy the service file
 
