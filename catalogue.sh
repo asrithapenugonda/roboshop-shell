c45a7bee9116c4e11225fc9d768e39c820f3a83d
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-11-06 11:26:10 +0000

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -3,23 +3,24 @@
 # Store the current working directory
 script_location=$(pwd)
 
+# Install the required packages
 sudo dnf install -y gcc-c++
 
 # Remove the existing Node.js (if any)
 sudo dnf remove nodejs -y
 
-# Install Node.js using NodeSource for the latest LTS version
-wget https://nodejs.org/dist/v18.0.0/node-v18.0.0-linux-x64.tar.xz
-tar -xf node-v18.0.0-linux-x64.tar.xz
-sudo cp -r node-v18.0.0-linux-x64/* /usr/local/
-rm -rf node-v18.0.0-linux-x64.tar.xz node-v18.0.0-linux-x64
+# Download and extract Node.js version 14
+wget https://nodejs.org/dist/v14.18.1/node-v14.18.1-linux-x64.tar.xz
+tar -xf node-v14.18.1-linux-x64.tar.xz
+sudo cp -r node-v14.18.1-linux-x64/* /usr/local/
+rm -rf node-v14.18.1-linux-x64.tar.xz node-v14.18.1-linux-x64
 
 # Check Node.js and npm versions
 node -v
 npm -v
 
 # Create a user for your application
-#sudo useradd roboshop
+sudo useradd roboshop
 
 # Create the application directory
 sudo mkdir -p /app
