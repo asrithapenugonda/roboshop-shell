ce3791a9773c10e4169529d8e16e63cc81a0dc3a
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-11-06 11:11:40 +0000

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,9 +1,7 @@
-
+#!/bin/bash
 
 # Store the current working directory
-
 script_location=$(pwd)
-
 
 # Install the required packages
 sudo dnf install -y gcc-c++
@@ -12,10 +10,12 @@ sudo dnf install -y gcc-c++
 sudo dnf remove nodejs -y
 
 # Download and extract Node.js
-wget https://nodejs.org/dist/v14.18.1/node-v14.18.1-linux-x64.tar.xz
-tar -xf node-v14.18.1-linux-x64.tar.xz
-sudo cp -r node-v14.18.1-linux-x64/* /usr/local/
-rm -rf node-v14.18.1-linux-x64.tar.xz node-v14.18.1-linux-x64
+node_version="v14.18.1"
+node_url="https://nodejs.org/dist/$node_version/node-$node_version-linux-x64.tar.xz"
+wget "$node_url"
+tar -xf "node-$node_version-linux-x64.tar.xz"
+sudo cp -r "node-$node_version-linux-x64/"* /usr/local/
+rm -rf "node-$node_version-linux-x64.tar.xz" "node-$node_version-linux-x64"
 
 # Check Node.js and npm versions
 node -v
@@ -28,16 +28,17 @@ sudo useradd roboshop
 sudo mkdir -p /app
 
 # Download the application code
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
-cd /app
-unzip /tmp/catalogue.zip
-
+app_code_url="https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip"
+sudo rm -rf /app/*
+sudo curl -L -o /tmp/catalogue.zip "$app_code_url"
+sudo unzip /tmp/catalogue.zip -d /app
 cd /app
 npm install
 
- cp ${script_location}/Files/catalogue.service /etc/systemd/system/catalogue.service
+# Copy the service file
+sudo cp "${script_location}/Files/catalogue.service" /etc/systemd/system/catalogue.service
+
 # Reload systemd and enable/start the service
- systemctl daemon-reload
- systemctl enable catalogue
- systemctl start catalogue
+sudo systemctl daemon-reload
+sudo systemctl enable catalogue
+sudo systemctl start catalogue
