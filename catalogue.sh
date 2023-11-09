7ecddf438b57685540d68452aee21af899ac0f51
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-11-09 10:29:01 +0000

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -14,21 +14,18 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 source ~/.bashrc
 nvm install node
 
-
-
 # Check Node.js and npm versions
 node -v
 npm -v
 
 # Create a user for your application
-#sudo useradd roboshop
+# sudo useradd roboshop
 
 # Create the application directory
 sudo mkdir -p /app
 
 # Download the application code
 app_code_url="https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip"
-
 sudo curl -L -o /tmp/catalogue.zip "$app_code_url"
 sudo rm -rf /app/*
 cd /app
@@ -44,7 +41,7 @@ sudo systemctl enable catalogue
 sudo systemctl start catalogue
 sudo systemctl restart catalogue
 
+# Copy MongoDB repository configuration
+sudo cp "${script_location}/Files/mongodb.repo" /etc/yum.repos.d/mongodb.repo
+sudo yum install mongodb-org-shell -y
 
-
-cp ${script_location}/Files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
