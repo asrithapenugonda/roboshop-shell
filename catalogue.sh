71c0b65bccb24177df7c62d084e75822700d6787
asrithapenugonda <asrithapenugonda16121@gmail.com>
2023-12-23 20:16:50 +0000

    updating ./roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -7,7 +7,7 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[34m Disable node js if present\e[0"
 dnf module disable nodejs -y &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
@@ -15,7 +15,7 @@ fi
 
 echo -e "\e[34m enable nodejs 18 version\e[0"
 dnf module enable nodejs:18 -y &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
@@ -23,7 +23,7 @@ fi
 
 echo -e "\e[34m installing Nodejs\e[0"
 dnf install nodejs -y &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
@@ -31,15 +31,15 @@ fi
 
 echo -e "\e[34m Create a user for your application\e[0"
 useradd roboshop &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
-   echo "Failure"
+  echo "Failure"
 fi
 
 echo -e "\e[34mCreate the application directory\e[0"
 mkdir -p /app &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
@@ -47,23 +47,21 @@ fi
 
 ecjo -e "\e[34mDownload the application code\e[0"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
 fi
-
 echo -e "\e[34m Changing directory to application\e[0"
  cd /app &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
 fi
-
 echo -e "\e[34m removing content in application\e[0"
 rm -rf /app/* &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
@@ -71,7 +69,7 @@ fi
 
 echo -e "\e[34m unzipping the file\e[0"
 unzip /tmp/catalogue.zip &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
@@ -81,15 +79,14 @@ fi
 
 echo -e "\e[34m Change to the application directory\e[0"
 cd /app &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
 fi
-
 echo -e "\e[34mInstall npm dependencies\e[0"
  npm install &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
@@ -97,55 +94,52 @@ fi
 
 echo -e "\e[34mCopy the service file\e[0"
  cp "${script_location}/Files/catalogue.service" /etc/systemd/system/catalogue.service &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
 fi
-
 echo -e "\e[34mReload systemd and enable/start the service\e[0"
 echo -e "\e[32m daemon reload\e[0"
 systemctl daemon-reload &>>{LOG}
-if ( $? -eq 0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
 fi
 echo -e "\e[34m enable catalogue\e[0"
 systemctl enable catalogue &>>{LOG}
-if ( $? -eq  0 ); then
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
 fi
 echo -e "\e[34m start catalogue \e[0"
- systemctl start catalogue &>>{LOG}
- if ( $? -eq 0 ); then
-   echo "success"
- else
-   echo "Failure"
- fi
-
-
-echo -e "\e[34mCopy MongoDB repository configuration\e[0"
- cp "${script_location}/Files/mongodb.repo" /etc/yum.repos.d/mongodb.repo &>>{LOG}
-if ( $? -eq 0 ); then
+systemctl start catalogue &>>{LOG}
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
 fi
 
+echo -e "\e[34mCopy MongoDB repository configuration\e[0"
+cp "${script_location}/Files/mongodb.repo" /etc/yum.repos.d/mongodb.repo &>>{LOG}
+if [ $? -eq 0 ]; then
+  echo "success"
+else
+  echo "Failure"
+fi
  echo -e "\e[34m installing mongodb\e[0"
- yum install mongodb-org-shell -y &>>{LOG}
-if ( $? -eq 0 ); then
+yum install mongodb-org-shell -y &>>{LOG}
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
 fi
 
 echo -e "\e[34m loading schema\e[0m"
- mongo --host localhost </app/schema/catalogue.js &>>{LOG}
-if ( $? -eq 0 ); then
+mongo --host localhost </app/schema/catalogue.js &>>{LOG}
+if [ $? -eq 0 ]; then
   echo "success"
 else
   echo "Failure"
