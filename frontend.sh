c435e8f4fdeb280d237ad708d71d17db3881842b
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-01-01 16:26:02 +0000

    updating ./roboshop-shell

--- frontend.sh
+++ frontend.sh
@@ -1,80 +1,47 @@
 script_loc=$(pwd)
 LOG=/tmp/roboshop.log
+satus_check() {
+  if [ $? -eq 0 ]; then
+    echo -e "\e[32msuccess\e[0m"
+  else
+    echo -e "\e[31mFailure\e[0m"
+  fi
+}
 
-echo -e "\e[33mInstalling nginx\e[0m"
+echo -e "\e[35mInstalling nginx\e[0m"
 yum install nginx -y &>>${LOG}
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
+status_check
 
-echo -e "\e[33mEnable nginx\e[0m"
+echo -e "\e[35mEnable nginx\e[0m"
 systemctl enable nginx &>>${LOG}
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
+status_check
 
-
-echo -e "\e[33mStarting Nginx\e[0m"
+echo -e "\e[35mStarting Nginx\e[0m"
 systemctl start nginx &>>${LOG}
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
-
-echo -e "\e[33mREmoving existing content in nginx\e[0m"
+status_check
+echo -e "\e[35mREmoving existing content in nginx\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
+status_check
 
 
-echo -e "\e[33mdownloading the zip file\e[0m"
+echo -e "\e[35mdownloading the zip file\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
+status_check
 
-
-echo -e "\e[33mmoving to the html directory\e[0m"
+echo -e "\e[35mmoving to the html directory\e[0m"
 cd /usr/share/nginx/html &>>${LOG}
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
+status_check
 
 
-echo -e "\e[33munxipping the frontend file\e[0m"
+echo -e "\e[35munxipping the frontend file\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
+status_check
 
 
-echo -e "\e[34mcalling out ngnix configuration\e[0m"
+echo -e "\e[35mcalling out ngnix configuration\e[0m"
 cp ${script_loc}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
+status_check
 
-echo -e "\e[34mrestarting nginx server[0m\e"
+echo -e "\e[35mrestarting nginx server[0m\e"
 systemctl restart nginx &>>${LOG}
-
-if [ $? -eq 0 ] ; then
-  echo "Success"
-else
-  echo "Failure"
-fi
+status_check
