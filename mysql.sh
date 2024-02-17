0739fb5972280ecd42a5e6f21d5c2f0ea6d4d6ae
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-02-17 11:41:01 +0000

    updating ./roboshop-shell

--- mysql.sh
+++ mysql.sh
@@ -1,29 +1,34 @@
 source common.sh
 
-if [ -z  "${root_mysql_password}" ] ; then
-  echo " no variable root_mysql_password"
-  exit
+if [ -z "${root_mysql_password}" ]; then
+  echo "Variable root_mysql_password is missing"
+  exit 1
 fi
-print_head "Disable mysql if present"
+
+
+print_head "Disable MySQL Default Module"
 dnf module disable mysql -y &>>${LOG}
 status_check
 
-print_head " Copying the repo file"
-cp ${script_location}/Files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
+print_head "Copy MySQL Repo file"
+cp ${script_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
 status_check
 
-print_head " Installing mysql"
-dnf install mysql-community-server -y  &>>${LOG}
+print_head "Install MySQL Server"
+yum install mysql-community-server -y  &>>${LOG}
 status_check
 
-print_head "enable  mysql"
-systemctl enable mysqld  &>>${LOG}
+print_head "Enable MySQL"
+systemctl enable mysqld &>>${LOG}
 status_check
 
-print_head " Start Mysql"
-systemctl start mysqld &>>${LOG}
+print_head "Start MySQL"
+systemctl restart mysqld &>>${LOG}
 status_check
 
-print_head "Reset default password"
-mysql_secure_installation --set-root-pass ${root_mysql_password} &>>{LOG}
+print_head "Reset Default Database Password"
+mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${LOG}
+if [ $? -eq 1 ]; then
+  echo "Password is already changed"
+fi
 status_check
