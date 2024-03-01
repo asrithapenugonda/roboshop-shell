6b9e64592c49ec29a22afde18132d25a2fbf126c
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-03-01 23:01:47 +0000

    updating ./roboshop-shell

--- payment.sh
+++ payment.sh
@@ -1,11 +1,11 @@
 source common.sh
 
-if [ -z "${root_rabbitmq_password}" ]; then
-  echo "variable root_rabbitmq_password is missing"
+component=payment
+schema_load=false
+
+if [ -z "${roboshop_rabbitmq_password}" ]; then
+  echo "Variable roboshop_rabbitmq_password is missing"
   exit 1
 fi
-
-component=payment
-schema_load=False
 
 PYTHON
