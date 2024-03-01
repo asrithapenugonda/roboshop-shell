d821043706b0c2e1ef386ff5dcbe1477b2ab114e
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-03-01 20:04:05 +0000

    updating ./roboshop-shell

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -26,9 +26,10 @@ systemctl start rabbitmq-server &>>${LOG}
 status_check
 
 print_head "adding user"
-sudo rabbitmqctl list_users grep | roboshop
-if [ $? -ne 0 ];then
-rabbitmqctl add_user roboshop ${root_rabbitmq_password} &>>${LOG}
+print_head "Add Application User"
+rabbitmqctl list_users | grep roboshop &>>${LOG}
+if [ $? -ne 0 ]; then
+  rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password}  &>>${LOG}
 fi
 status_check
 
