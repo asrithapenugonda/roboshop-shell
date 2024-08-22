f9006597db292058e21d7c08913d9666dadeff50
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-08-22 15:19:47 +0100

    mongodb commit

--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,5 @@
 script_location=${pwd}
 
-
 cp ${script_location}/Files/mongod.repo /etc/yum.repos.d/mongodb.repo
 
 dnf install mongodb-org -y
@@ -8,5 +7,5 @@ dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 
-sed -e 's/127.0.0.0/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
