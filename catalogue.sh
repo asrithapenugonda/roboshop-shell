af692d6bff83fccd0c20fe1c53d7f69d4b240ff0
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-08-30 17:57:17 +0100

    catalogue 2nd commit

--- catalogue.sh
+++ catalogue.sh
@@ -16,7 +16,7 @@ npm install
 cp ${script_location}/Files/catalogue.service /etc/systemd/system/catalogue.service
 
 
-cp ${script_location}/File/mongod.repo /etc/yum.repos.d/mongodb.repo
+cp ${script_location}/Files/mongod.repo /etc/yum.repos.d/mongodb.repo
 
 dnf install mongodb-org -y
 mongo --host mongodb-dev.robomart.tech </app/schema/catalogue.js
