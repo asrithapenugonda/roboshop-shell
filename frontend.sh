83da2ba5c5d23c4c73f125782fcaf8b6c2e72308
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-08-30 17:32:35 +0100

    comments to nginx

--- frontend.sh
+++ frontend.sh
@@ -1,25 +1,25 @@
 script_location=$(pwd)
 
-echo -e "\e[32m installing nginx \e[0m"
+echo -e "\e[31m installing nginx \e[0m"
 dnf install nginx -y
 
-echo -e "\e[32m enabling nginx \e[0m"
+echo -e "\e[31m enabling nginx \e[0m"
 systemctl enable nginx
-echo -e "\e[32m starting the nginx \e[0m"
+echo -e "\e[31m starting the nginx \e[0m"
 systemctl start nginx
 
-echo -e "\e[32m removing the default content \e[0m"
+echo -e "\e[31m removing the default content \e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "\e[32m downloading our frontend zip file \e[0m"
+echo -e "\e[31m downloading our frontend zip file \e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-echo -e "\e navigating to the desired file "
+echo -e "\e[31mnavigating to the desired file \e[0m"
 cd /usr/share/nginx/html
-echo -e "/e[32m unzipping the frontend content"
+echo -e "\e[31m unzipping the frontend content\e[0m"
 unzip /tmp/frontend.zip
 
-echo -e "/e[32m creating roboshop configuration \e[0m"
+echo -e "\e[31m creating roboshop configuration \e[0m"
 cp ${script_location}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[32m restarting the nginx\e[0m"
+echo -e "\e[31m restarting the nginx\e[0m"
 systemctl restart nginx
