f75399dde5943ce27a19bddcbc7f66ebfc4693b7
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-08-30 17:36:27 +0100

    mongodb creation

--- mongodb.sh
+++ mongodb.sh
@@ -1,2 +1,14 @@
 script_location=$(pwd)
 
+cp ${script_location}/Files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+dnf install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
+systemctl restart mongod
+
+
