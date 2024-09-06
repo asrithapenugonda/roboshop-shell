fcabfe3ad56514000d51cd0ce13c7b517f45aa10
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-09-06 10:35:56 +0100

    nginx enhanced commit

--- frontend.sh
+++ frontend.sh
@@ -4,9 +4,9 @@ Log=/tmp/roboshop.log
 status_check () {
   if [ $? -eq 0 ]
   then
-    echo "Success"
+    echo -e "\e[32mSuccess\e[0m"
   else
-    echo "Failure"
+    echo "\e[31mFailure\e[0m"
     echo "Refer to the file ${LOG}"
     exit
   fi
