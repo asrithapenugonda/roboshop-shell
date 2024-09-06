4369b31591b5169f28411c61be490741e79b6b59
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-09-06 11:10:35 +0100

    nginx enhanced commit

--- common.sh
+++ common.sh
@@ -4,10 +4,14 @@ Log=/tmp/roboshop.log
 status_check () {
   if [ $? -eq 0 ]
   then
-    echo -e "\e[32mSuccess\e[0m"
+    echo -e "\e[1;32mSUCCESS\e[0m"
   else
-    echo "\e[31mFailure\e[0m"
+    echo "\e[1;31mFAILURE\e[0m"
     echo "Refer to the file ${LOG}"
     exit
   fi
+}
+
+print_head () {
+  echo -e "\e[40m $1 \e[0m"
 }
--- frontend.sh
+++ frontend.sh
@@ -1,37 +1,37 @@
 source common.sh
 
-echo -e "\e[45m installing nginx \e[0m"
+print_head  "Installing Nginx"
 dnf install nginx -y &>>${Log}
 status_check
 
-echo -e "\e[45m enabling nginx \e[0m"
+print_head "Enabling Nginx"
 systemctl enable nginx &>>${Log}
 status_check
 
-echo -e "\e[45m starting the nginx \e[0m"
+print_head "Starting the Nginx"
 systemctl start nginx &>>${Log}
 status_check
 
-echo -e "\e[45m removing the default content \e[0m"
+print_head "Removing the default nginx content"
 rm -rf /usr/share/nginx/html/* &>>${Log}
 status_check
 
-echo -e "\e[45m downloading our frontend zip file \e[0m"
+print_head "Downloading our frontend zip file"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${Log}
 status_check
 
-echo -e "\e[45m navigating to the desired file \e[0m"
+print_head "Navigating to the desired file"
 cd /usr/share/nginx/html &>>${Log}
 status_check
 
-echo -e "\e[45m unzipping the frontend content\e[0m"
+print_head "Unzipping the frontend content"
 unzip /tmp/frontend.zip &>>${Log}
 status_check
 
-echo -e "\e[45m creating roboshop configuration \e[0m"
+print_head "Creating roboshop configuration"
 cp ${script_location}/Files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${Log}
 status_check
 
-echo -e "\e[45m restarting the nginx\e[0m"
+print_head "Restarting the nginx"
 systemctl restart nginx &>>${Log}
 status_check
