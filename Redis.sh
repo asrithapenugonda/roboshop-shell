df3c6c038557ac5154d684388e3b384f5ebcd87f
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-09-06 17:53:31 +0100

    enhanced script

---
+++ Redis.sh
@@ -0,0 +1,25 @@
+source common.sh
+
+print_head "Setiing Up Redis Repo"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>{LOG}
+status_check
+
+print_head "Enabling Redis 6.2 version"
+dnf module enable redis:remi-6.2 -y &>>{LOG}
+status_check
+
+print_head "Installing Redis"
+dnf install redis -y  &>>{LOG}
+status_check
+
+print_head "Changing the Listen address"
+sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/redis.conf etc/redis/redis.conf  &>>{LOG}
+status_check
+
+print_head "Enable Redis"
+systemctl enable redis &>>{LOG}
+status_check
+
+print_head "Start Redis"
+systemctl start redis &>>{LOG}
+status_check
