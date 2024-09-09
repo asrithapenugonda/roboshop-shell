14749709a53b284ac5e6c380a49dbb573c525ed5
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-09-10 00:36:03 +0100

    mysql commit

--- mysql.sh
+++ mysql.sh
@@ -1,31 +1,29 @@
 source common.sh
 
-if [ -z "${root_mysql_password}" ]
-then
-  echo "root_mysql_password is missing"
-else
+if [ -z "${root_mysql_password}" ]; then
+  echo "Variable root_mysql_password is missing"
   exit 1
 fi
 
-print_head " Disable the existing Mysql"
-dnf module disable mysql -y  &>>${LOG}
+
+print_head "Disable MySQL Default Module"
+dnf module disable mysql -y &>>${LOG}
 status_check
 
-print_head " Copying the repo file"
+print_head "Copy MySQL Repo file"
 cp ${script_location}/Files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
 status_check
 
-
-print_head "Installing mysql"
-dnf install mysql-community-server -y &>>${LOG}
+print_head "Install MySQL Server"
+yum install mysql-community-server -y  &>>${LOG}
 status_check
 
-print_head "Enable Mysql"
+print_head "Enable MySQL"
 systemctl enable mysqld &>>${LOG}
 status_check
 
-print_head "Start Mysql"
-systemctl start mysqld  &>>{LOG}
+print_head "Start MySQL"
+systemctl restart mysqld &>>${LOG}
 status_check
 
 print_head "Reset Default Database Password"
