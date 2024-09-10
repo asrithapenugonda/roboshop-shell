4fa24da41db4df4ad4ba1777b3923622935dad2e
asrithapenugonda <asrithapenugonda16121@gmail.com>
2024-09-10 01:29:36 +0100

    Rabbitmq commit

---
+++ rabbitmq.sh
@@ -0,0 +1,38 @@
+source common.sh
+
+if [ -z "${roboshop_rabbitmq_password}" ]; then
+  echo "Variable root_mysql_password is missing"
+  exit 1
+fi
+
+
+print_head "Downloading Erlang Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>>${LOG}
+status_check
+
+print_head "Downloading Erlang Repos"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>>${LOG}
+status_check
+
+print_head " Installing RabbitMQ"
+dnf install rabbitmq-server -y  &>>${LOG}
+status_check
+
+print_head "Enable RabbitMQ"
+systemctl enable rabbitmq-server &>>${LOG}
+status_check
+
+print_head "Start RabbitMQ"
+systemctl start rabbitmq-server  &>>${LOG}
+status_check
+
+print_head ' Adding application user'
+rabbitmqctl list users | grep roboshop  &>>{LOG}
+if [ $? != 0 ] ; then
+  rabbitmqctl add_user roboshop ${roboshop_rabbitmq_password} &>>${LOG}
+fi
+status_check
+
+print_head "Adding Permissions"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"    &>>${LOG}
+status_check
