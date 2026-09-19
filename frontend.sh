143c2c7eae42903e2aa3251fe28b5bc11bff4174
asrithapenugonda <asrithapenugonda16121@gmail.com>
2026-09-19 13:49:45 +0100

    frontend script

--- frontend.sh
+++ frontend.sh
@@ -11,6 +11,6 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-cp files/nginx-roboshop.conf /etc/nginx/default.d/robohsop.conf
+cp files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 system restart nginx
 #frontend script
